90fc387ae25cc5133944296e6bddc675553a962b
wckdhunter <43898742+wckdhunter@users.noreply.github.com>
2019-05-14 22:43:38 +0700

    Update insert.php

--- www/insert.php
+++ www/insert.php
@@ -2,11 +2,11 @@
  include "db.php";
  if(isset($_POST['insert']))
  {
- $nama_makanan=$_POST['nama_makanan'];
- $harga=$_POST['harga'];
- $stok=$_POST['stok'];
+ $nama_penjual=$_POST['nama_penjual'];
+ $nama_warung=$_POST['nama_warung'];
+ $nohp=$_POST['nohp'];
  $id_penjual=$_POST['id_penjual'];
- $q=mysqli_query($con,"INSERT INTO `makanan` (`nama_makanan`,`harga`,`stok`,`id_penjual`) VALUES ('$nama_makanan','$harga','$stok','$id_penjual')");
+ $q=mysqli_query($con,"INSERT INTO `penjual` (`nama_penjual`,`nama_warung`,`nohp`,`id_penjual`) VALUES ('$nama_penjual','$nama_warung','$nohp','$id_penjual')");
  if($q)
   echo "success";
  else
